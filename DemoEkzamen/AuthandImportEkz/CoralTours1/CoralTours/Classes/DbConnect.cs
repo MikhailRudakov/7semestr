9a1250f32b11c4c7a13004beedb11310a626c930
Dmitriy Merinov <takistproaz123@gmail.com>
2022-11-26 17:46:54 +0300

    daga

---
+++ DemoEkzamen/AuthandImportEkz/CoralTours1/CoralTours/Classes/DbConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoralTours.Classes
+{
+    class DbConnect
+    {
+        public static Models.HotelRud modeldb;
+    }
+}
