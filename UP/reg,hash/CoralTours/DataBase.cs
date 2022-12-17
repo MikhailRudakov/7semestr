62b913e8021cc994377345fc828a15462b450655
MikhailRudakov <90384405+MikhailRudakov@users.noreply.github.com>
2022-12-17 13:54:52 +0300

    Update DataBase.cs

--- UP/reg,hash/CoralTours/DataBase.cs
+++ UP/reg,hash/CoralTours/DataBase.cs
@@ -5,12 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 
-namespace GachiTours
+namespace CoralTours
 {
     internal class DataBase
     {
 
-        SqlConnection sqlConnection = new SqlConnection(@"Data Source=KAHAILMILASH\SQLEXPRESS; Initial Catalog=GachiToursMD; Integrated Security=True");
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=RUDAKOVPC\SQLEXPRESS; Initial Catalog=CoralTours; Integrated Security=True");
 
         public void openConnection()
         {
